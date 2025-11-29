3406fe5c2066babb4fc5e8dc2c9e7cac57e6b616
Zoe Huang <zxh230016@utdallas.edu>
2025-11-29 17:32:43 -0600

    Set FPS

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import random
 import pygame
 
+
+FPS = 60
 #initializing game and creating window
 pygame.init()
 screen = pygame.display.set_mode((500,600))
+clock = pygame.time.Clock()
 
 running = True
 
 #game loop
 while running:
+    clock.tick(FPS)
     #get input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
