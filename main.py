04dd6ece0b0630fb55bcf555bc7484927525c527
Zoe Huang <zxh230016@utdallas.edu>
2025-11-29 18:23:39 -0600

    Set character and display

--- main.py
+++ main.py
@@ -3,14 +3,31 @@ import pygame
 
 
 FPS = 60
+WIDTH = 500
+HEIGHT = 600
+
 #initializing game and creating window
 pygame.init()
-screen = pygame.display.set_mode((500,600))
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
-running = True
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill((0, 255, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = 200
+        self.rect.y = 200
+
+all_sprite = pygame.sprite.Group()
+player = Player()
+all_sprite.add(player)
 
 #game loop
+running = True
+
+
 while running:
     clock.tick(FPS)
     #get input
@@ -20,5 +37,7 @@ while running:
     #update
 
     #display
+    all_sprite.draw(screen)
+    pygame.display.update()
 
 pygame.QUIT
