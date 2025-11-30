d318fde6998020f9a669a6290ac2ec15f19577ac
Zoe Huang <zxh230016@utdallas.edu>
2025-11-29 19:03:48 -0600

    Add WASD keyboard control

--- main.py
+++ main.py
@@ -17,8 +17,30 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((50, 40))
         self.image.fill((0, 255, 0))
         self.rect = self.image.get_rect()
-        self.rect.x = 200
-        self.rect.y = 200
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 8
+        self.speedy = 8
+
+    def update(self):
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[pygame.K_d]:
+            self.rect.x += self.speedx
+        if key_pressed[pygame.K_a]:
+            self.rect.x -= self.speedx
+        if key_pressed[pygame.K_w]:
+            self.rect.y -= self.speedy
+        if key_pressed[pygame.K_s]:
+            self.rect.y += self.speedy
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+
 
 all_sprite = pygame.sprite.Group()
 player = Player()
@@ -35,8 +57,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     #update
+    all_sprite.update()
 
     #display
+    screen.fill((0, 0, 0))
     all_sprite.draw(screen)
     pygame.display.update()
 
